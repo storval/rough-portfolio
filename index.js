30d5b2a350df1bd38b8b5ce647da9b101cc30a8d
Ar on <aronbjarklind@gmail.com>
2022-10-15 20:50:20 +0000

    minor fixes

---
+++ index.js
@@ -0,0 +1,25 @@
+// function enlargeImg(){
+//     let theImage = document.getElementsByClassName("mynd1");
+//     theImage.width = theImage.width * 2;
+//     theImage.height = theImage.height * 2;
+
+
+//   };
+
+
+//     theImage = docuument.getElementsByClassName('')
+
+
+//   window.onload = function() {
+//     let anchors = document.getElementsByClassName('mynd1');
+//     for(var i = 0; i < anchors.length; i++) {
+//         var anchor = anchors[i];
+//         anchor.onclick = function() {
+//             theImage.width = theImage.width * 2;
+    
+//             theImage.height = theImage.height * 2;
+//         }
+//     }
+// }
+
+// soon...
